8221f41fe72e737bf95e9555f5b9fe2656003c7a
matansuliman <matan.2001@gmail.com>
2025-12-16 21:47:09 +0200

    add back-to-top button with smooth scroll functionality

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ document.addEventListener("DOMContentLoaded", function () {
         yearElement.textContent = new Date().getFullYear();
     }
 });
+
+
+const backToTopBtn = document.getElementById("backToTop");
+const heroSection = document.querySelector("header"); // ה־Hero שלך
+
+window.addEventListener("scroll", () => {
+    const heroBottom = heroSection.offsetHeight;
+
+    if (window.scrollY > heroBottom - 50) {
+        backToTopBtn.classList.add("show");
+    } else {
+        backToTopBtn.classList.remove("show");
+    }
+});
+
+backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
